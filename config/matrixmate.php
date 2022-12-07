1c56bc54b3893f1a440ca5f89c3f91d2b828e462
CreateSean <sean@caffeinecreations.ca>
2022-12-07 10:49:54 -0500

    database dump

--- config/matrixmate.php
+++ config/matrixmate.php
@@ -1,64 +1,65 @@
 <?php
 
+$globalConfig = [
+  'defaultTabName' => 'Content',
+  'defaultTabFirst' => true,
+  'groups' => [
+    [
+      'label' => 'Text Heavy',
+      'types' => ['copy'],
+    ],
+    [
+      'label' => 'Related',
+      'types' => ['relatedEntries', 'fragments'],
+    ],
+    [
+      'label' => 'Other',
+      'types' => ['callToAction', 'video', 'codeEmbed'],
+    ],
+  ],
+  'types' => [
+    'copy' => [
+      'tabs' => [
+        [
+          'label' => 'Settings',
+          'fields' => ['padding'],
+        ]
+      ],
+    ],
+    'video' => [
+      'tabs' => [
+        [
+          'label' => 'Settings',
+          'fields' => ['padding'],
+        ]
+      ],
+    ],
+    'relatedEntries' => [
+      'tabs' => [
+        [
+          'label' => 'Settings',
+          'fields' => ['padding'],
+        ]
+      ],
+    ],
+    'codeEmbed' => [
+      'tabs' => [
+        [
+          'label' => 'Settings',
+          'fields' => ['padding'],
+        ]
+      ],
+    ]
+  ],
+];
+
   return [
     'fields' => [
       'contentBuilder' => [
-        'defaultTabName' => 'Content',
-        'defaultTabFirst' => true,
-        'groups' => [
-          [
-            'label' => 'Text Heavy',
-            'types' => ['copy'],
-          ],
-          [
-            'label' => 'Related',
-            'types' => ['relatedEntries', 'fragments'],
-          ],
-          [
-            'label' => 'Other',
-            'types' => ['callToAction', 'video', 'codeEmbed'],
-          ],
-        ],
-        'types' => [
-          'copy' => [
-            'tabs' => [
-              [
-                'label' => 'Settings',
-                'fields' => ['padding'],
-              ]
-            ],
-          ],
-          'video' => [
-            'tabs' => [
-              [
-                'label' => 'Settings',
-                'fields' => ['padding'],
-              ]
-            ],
-          ],
-          'relatedEntries' => [
-            'tabs' => [
-              [
-                'label' => 'Settings',
-                'fields' => ['padding'],
-              ]
-            ],
-          ],
-          'codeEmbed' => [
-            'tabs' => [
-              [
-                'label' => 'Settings',
-                'fields' => ['padding'],
-              ]
-            ],
-          ]
-        ],
-
-        // groups and types need to be copied from above
-        // to apply to the fragments channel.
-        'section:fragments' => [
+        '*' => $globalConfig,
+        'section:fragments' => array_merge_recursive($globalConfig, [
           'hiddenTypes' => ['fragments'],
-        ],
+        ]),
       ],
     ],
   ];
