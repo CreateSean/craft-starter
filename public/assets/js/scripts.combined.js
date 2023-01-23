a088c07b8012e133ffbd42c0a53a83129dfa930d
CreateSean <sean@caffeinecreations.ca>
2023-01-23 10:59:24 -0500

    animated hamburger menu

--- public/assets/js/scripts.combined.js
+++ public/assets/js/scripts.combined.js
@@ -1,7 +1,7 @@
 /**!
  * @project        Caffeine Creations Website
  * @author         Sean Smith, Caffeine Creations <sean@caffeinecreations.ca>
- * @Copyright      Copyright (c) 2022, Caffeine Creations
+ * @Copyright      Copyright (c) 2023, Caffeine Creations
  *
  */
 
