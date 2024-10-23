0625ce747fa58e7ee2262a227cde16e32831fac3
CreateSean <sean@caffeinecreations.ca>
2024-10-23 13:27:22 -0400

    updated craft and plugins (5.4.9)

--- templates/_javascript/hero-swiper.js
+++ templates/_javascript/hero-swiper.js
@@ -1,1 +1,13 @@
-c
+const swiper = new Swiper('.hero-swiper', {
+  // Optional parameters
+  loop: true,
+
+  // pagination: {
+  //   el: ".home-pagination",
+  //   clickable:true,
+  // },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
